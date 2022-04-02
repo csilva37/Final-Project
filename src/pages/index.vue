605ceefa188a3853558ec552dd078be228737145
csilva37 <csilva37@stu.southtexascollege.edu>
2022-04-01 19:46:59 -0500

    Added Real Login

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,18 +5,19 @@ const { isAuthenticated, user } = useAuth();
 
 <template>
   <h1 class="text-4xl">Home Page</h1>
+   <h2
+      v-if="!isAuthenticated"
+      class="text-4xl tracking-tighter font-extralight"
+    >
+      Welcome
+    </h2>
+    <h2 v-else class="text-4xl tracking-tighter font-extralight">
+      Welcome {{ user }}
+    </h2>
 <p class="text-xl"> Your one stop shop for all of your character's needs!</p> 
  <div
     class="flex flex-col items-center justify-center space-y-12  min-h-screen-nonav"
   >
-    <h2
-      v-if="!isAuthenticated"
-      class="text-6xl tracking-tighter font-extralight"
-    >
-      Welcome
-    </h2>
-    <h2 v-else class="text-6xl tracking-tighter font-extralight">
-      Welcome {{ user }}
-    </h2>
+   
  </div>
 </template>
