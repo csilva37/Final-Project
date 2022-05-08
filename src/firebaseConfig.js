133cefaf77d63c03d3027d4be0af8766416b5a31
csilva37 <csilva37@stu.southtexascollege.edu>
2022-05-08 14:58:27 -0500

    Merge

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDooimMHAqX6lB98Q9x8ZA9X87gw1zXeOs",
+  authDomain: "final-project-837b1.firebaseapp.com",
+  projectId: "final-project-837b1",
+  storageBucket: "final-project-837b1.appspot.com",
+  messagingSenderId: "349861438884",
+  appId: "1:349861438884:web:4fdc3ef3e068f310488d31"
+
+};
